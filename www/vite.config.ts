aa022c1023dc1300eb97039e403f8ee164d3a80f
Kian Kasad <kian@kasad.com>
2025-07-23 22:33:48 -0700

    fix(www): fix site build config issues

--- www/vite.config.ts
+++ www/vite.config.ts
@@ -3,6 +3,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
     plugins: [tailwindcss()],
+    base: "./",
     server: {
         fs: {
             // Allow serving files from the repository root

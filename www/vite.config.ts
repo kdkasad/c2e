76727c961f31c35fd4807e5cb4448a77a9492d87
Kian Kasad <kian@kasad.com>
2025-07-23 21:00:48 -0700

    style: install and run prettier

--- www/vite.config.ts
+++ www/vite.config.ts
@@ -1,14 +1,12 @@
-import { defineConfig } from 'vite';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-    plugins: [
-        tailwindcss(),
-    ],
+    plugins: [tailwindcss()],
     server: {
         fs: {
             // Allow serving files from the repository root
-            allow: ['..']
-        }
-    }
-})
+            allow: [".."],
+        },
+    },
+});
